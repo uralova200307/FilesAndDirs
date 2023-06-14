953087742e9526a2022ebeca6df039c2ac3418ce
zarina <zarinauralova0307@gmail.com>
2023-06-14 08:49:37 +0500

    Update Program.cs

--- Files and Dirs/Files and Dirs/Program.cs
+++ Files and Dirs/Files and Dirs/Program.cs
@@ -4,22 +4,7 @@
     {
         static void Main(string[] args)
         {
-            #region Task 2
-
-            string fullPath = Directory.GetCurrentDirectory();
-            DirectoryInfo directoryInfo = new DirectoryInfo(fullPath).Parent.Parent.Parent.Parent;
-            string[] directories = Directory.GetDirectories(directoryInfo.FullName, "*", SearchOption.AllDirectories);
-
-            foreach (string directory in directories)
-            {
-                string folderName = Path.GetFileName(directory);
-                if (folderName == "bin" || folderName == "obj")
-                {
-                    Console.WriteLine(directory);
-                }
-            }
-
-            #endregion
+            Console.WriteLine("Hello, World!");
         }
     }
 }
