4413e267e5ae57aa38a5aa8a72f535b898f3c1ff
zarina <zarinauralova0307@gmail.com>
2023-06-14 08:46:23 +0500

    Task 2

--- Files and Dirs/Files and Dirs/Program.cs
+++ Files and Dirs/Files and Dirs/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Task 2
+
+            string fullPath = Directory.GetCurrentDirectory();
+            DirectoryInfo directoryInfo = new DirectoryInfo(fullPath).Parent.Parent.Parent.Parent;
+            string[] directories = Directory.GetDirectories(directoryInfo.FullName, "*", SearchOption.AllDirectories);
+
+            foreach (string directory in directories)
+            {
+                string folderName = Path.GetFileName(directory);
+                if (folderName == "bin" || folderName == "obj")
+                {
+                    Console.WriteLine(directory);
+                }
+            }
+
+            #endregion
         }
     }
 }
