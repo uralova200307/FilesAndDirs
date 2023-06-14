a478099daae4eee1165d5162b9861d7de28e5470
zarina <zarinauralova0307@gmail.com>
2023-06-14 08:55:43 +0500

    Task 4

--- Files and Dirs/Files and Dirs/Program.cs
+++ Files and Dirs/Files and Dirs/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Task 4
+
+            string path = @"C:\Users\Miraziz_Khidoyatov\Documents\Pdp\Module-03\Classes\Lesson06\Lesson06";
+            string[] directories = { "bin", "obj" };
+
+            foreach (string directory in directories)
+            {
+                string folderPath = Path.Combine(path, directory);
+
+                if (Directory.Exists(folderPath))
+                {
+                    Directory.Delete(folderPath, true);
+                    Console.WriteLine($"{folderPath} o'chirildi.");
+                }
+                else
+                {
+                    Console.WriteLine($"{folderPath} topilmadi.");
+                }
+            }
+
+            #endregion
         }
     }
 }
